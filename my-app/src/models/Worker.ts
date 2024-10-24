15bad6c08fa75b94cd32e5fa66704725cf6f5fcb
caliew <caliew888@gmail.com>
2024-10-24 13:53:39 +0800

    Refactor Clean up on Front End

--- my-app/src/models/Worker.ts
+++
@@ -1,6 +0,0 @@
-// Worker.ts
-export interface Worker {
-    id: number;
-    name: string;
-    restaurantId: number;
-}
--- my-app/src/redux/store.ts
+++ my-app/src/redux/store.ts
@@ -1,14 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import workersReducer from './features/workersSlice';
-import restaurantsReducer from './features/restaurantsSlice';
 import visitorsReducer from './features/visitorsSlice';
 import faciltiesReducer from './features/facilitiesSlice';
 import bookingsReducer from './features/bookingsSlice';
 
 const store = configureStore({
   reducer: {
-    workers: workersReducer,
-    restaurants: restaurantsReducer,
     visitors: visitorsReducer,
     facilities: faciltiesReducer,
     bookings: bookingsReducer
