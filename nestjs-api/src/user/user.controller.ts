4513a1a81e6c42bece8eb1f12d5c86d3a0edccf1
caliew <caliew888@gmail.com>
2024-11-24 22:19:14 +0800

    Custom Guard

--- nestjs-api/src/user/user.controller.ts
+++ nestjs-api/src/user/user.controller.ts
@@ -1,14 +1,15 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
+import { JwtGuard } from 'src/auth/guard';
 
 @Controller('users')
 export class UserController {
 
-    @UseGuards(AuthGuard('jwt'))
+    @UseGuards(JwtGuard)
     @Get('test')
     test(@Req() req: Request) {
         console.log(req.user);
-        return 'Test Function on Users'
+        return req.user;
     }
 }
