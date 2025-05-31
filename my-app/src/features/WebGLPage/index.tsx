1282f7a5c2cc3993123fc31ce1cb7889908366cc
caliew <caliew888@gmail.com>
2025-05-31 17:09:19 +0800

    DATLoader modules and sample BDF Models

--- my-app/src/features/WebGLPage/index.tsx
+++ my-app/src/features/WebGLPage/index.tsx
@@ -1,4 +1,6 @@
 import { WebGLComponentContainer } from "@shared/components/WebGLComponent";
+import { SlideShow, PageAction } from "@shared/components";
+import { PageClasses } from "@shared/utils/classname";
 import "./index.css";
 
 const WebGLPage = () => {
@@ -6,6 +8,7 @@ const WebGLPage = () => {
     <div className="WebGLPage">
       <h1>WebGL Page</h1>
       <WebGLComponentContainer />
+      <PageAction />
     </div>
   );
 };
